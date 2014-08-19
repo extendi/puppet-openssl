5fea44d958d22868843d673e1bb7ded08a578ab4
Matteo Alessani <alessani@gmail.com>
2014-08-19 18:04:22 +0200

    fixed

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,14 +7,14 @@ class openssl {
     include_src => false,
   }
 
-  $openssl_pkgs = ["openssl", "libssl1.0.0", "libssl1.0.0-dbg", "libssl-dev", "libssl-doc", ]
+  $openssl_pkgs = [openssl", "libssl1.0.0", "libssl1.0.0-dbg", "libssl-dev", "libssl-doc", ]
 
   apt::hold { $openssl_pkgs:
     version => '1.0.1f-1ubuntu2.6',
   }
 
   package { $openssl_pkgs:
-    ensure => 'installed',
+    ensure => 'absent',
   }
 
 }
