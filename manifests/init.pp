32279b6d51ac39c90af6f50e4913511ecdbac18f
Matteo Alessani <alessani@gmail.com>
2014-08-19 17:30:47 +0200

    updated repo

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,10 +11,33 @@ class openssl {
 
   apt::hold { $openssl_pkgs:
     version => '1.0.1f-1ubuntu2.6',
+    notify => Package['install open ssl']
   }
 
-#  package { $openssl_pkgs:
-#    ensure => 'installed'
-#  }
+  package { "install open ssl":
+    name => "openssl"
+    ensure => 'installed'
+  }
+
+  package { "install open ssl1":
+    name => "libssl1.0.0"
+    ensure => 'installed'
+  }
+
+  package { "install open ssl dbg":
+    name => "libssl1.0.0-dbg"
+    ensure => 'installed'
+  }
+
+  package { "install open ssl dev":
+    name => "libssl-dev"
+    ensure => 'installed'
+  }
+  package { "install open ssl doc":
+    name => "libssl-doc"
+    ensure => 'installed'
+  }
+
+  Package['install open ssl'] -> Package['install open ssl1'] -> Package['install open ssl dbg'] -> Package['install open ssl dev'] -> Package['install open ssl doc']
 
 }
